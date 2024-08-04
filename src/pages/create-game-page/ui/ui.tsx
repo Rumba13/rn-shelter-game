44a1a29e12cbc3243d20f152d8b14e378d641907
Rumba13 <cveremeev@yandex.by>
2024-08-04 09:37:00 +0300

    fix: fix lagging navigation between screens

--- src/pages/create-game-page/ui/ui.tsx
+++ src/pages/create-game-page/ui/ui.tsx
@@ -1,6 +1,6 @@
 import { ImageBackground, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { Footer } from '@/src/shared/ui/footer/ui';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Header } from './header/ui';
 import { GameOptionCheckbox } from '@/src/pages/create-game-page/ui/game-option-checkbox/ui';
 import { GameOptionSelect } from '@/src/pages/create-game-page/ui/game-option-select/ui';
@@ -24,10 +24,16 @@ import {
   characteristicCards,
   characteristicCardsList,
 } from '@/src/entities/characteristic-card/model/characteristic-card';
-import { renderCharacteristicCardSelectedText } from '@/src/pages/create-game-page/ui/render-characteristic-card-selected-text';
+import {
+  renderCharacteristicCardSelectedText,
+} from '@/src/pages/create-game-page/ui/render-characteristic-card-selected-text';
 import { CharacteristicCardsList } from '@/src/shared/lib/types/characteristic-cards-list';
-import { characteristicCardNameToCard } from '@/src/entities/characteristic-card/model/characteristic-card-name-to-card';
-import { characteristicBalanceValueToTitle } from '@/src/pages/create-game-page/ui/characteristic-balance-value-to-title';
+import {
+  characteristicCardNameToCard,
+} from '@/src/entities/characteristic-card/model/characteristic-card-name-to-card';
+import {
+  characteristicBalanceValueToTitle,
+} from '@/src/pages/create-game-page/ui/characteristic-balance-value-to-title';
 import { createGameStore } from '@/src/feature/create-game/model/create-game';
 import { gameStore } from '@/src/entities/game/model/game';
 import { OverlayModal } from '@/src/shared/ui/overlay-modal/ui';
@@ -44,8 +50,13 @@ type PropsType = {
 export const CreateGamePage = observer(({ navigation }: PropsType) => {
   const [isErrorModalOpened, setIsErrorModalOpened] = useState<boolean>(false);
   const [errorDescription, setErrorDescription] = useState<string | null>(null);
+  const [isPageFullLoadingStarted, setIsPageFullLoadingStarted] = useState<boolean>(false);
 
   const { settings } = gameSettingsStore;
+
+  useEffect(() => {
+    setIsPageFullLoadingStarted(true);
+  }, []);
 
   return (
     <View style={s.createGamePageWrapper}>
@@ -97,101 +108,107 @@ export const CreateGamePage = observer(({ navigation }: PropsType) => {
                     gameSettingsStore.setSettings(options => (options.sexualOrientation = orientation))
                   }
                 />
-                <GameOptionRange
-                  title={'Уровень сложности'}
-                  defaultValue={settings.difficulty}
-                  description={'Параметр определяет насколько персонажи полезны и безопасны в среднем'}
-                  descriptionHeight={100}
-                  selectedTitle={difficultyValueToTitle(gameSettingsStore.settings.difficulty)}
-                  onValueChanged={difficulty =>
-                    gameSettingsStore.setSettings(options => (options.difficulty = difficulty))
-                  }
-                  min={gameSettingsStore.settingsLimits.difficulty.min}
-                  max={gameSettingsStore.settingsLimits.difficulty.max}
-                />
-                <GameOptionRange
-                  title={'Баланс Персонажей'}
-                  descriptionHeight={100}
-                  description={'Параметр определяет насколько различается полезность персонажей'}
-                  onValueChanged={characterBalance =>
-                    gameSettingsStore.setSettings(options => (options.balance = characterBalance))
-                  }
-                  selectedTitle={characterBalanceValueToTitle(settings.balance)}
-                  min={gameSettingsStore.settingsLimits.balance.min}
-                  max={gameSettingsStore.settingsLimits.balance.max}
-                  defaultValue={settings.balance}
-                />
-                <GameOptionRange
-                  title={'Баланс характеристик'}
-                  descriptionHeight={70}
-                  description={'Параметр определяет разброс характеристик'}
-                  onValueChanged={characteristicBalance =>
-                    gameSettingsStore.setSettings(settings => (settings.characteristicBalance = characteristicBalance))
-                  }
-                  selectedTitle={characteristicBalanceValueToTitle(settings.characteristicBalance)}
-                  min={gameSettingsStore.settingsLimits.characteristicBalance.min}
-                  max={gameSettingsStore.settingsLimits.characteristicBalance.max}
-                  defaultValue={settings.characteristicBalance}
-                />
 
-                <GameOptionList<ShelterCategoryList>
-                  title={'Список бункеров'}
-                  descriptionHeight={100}
-                  description={'Выберите из списка, с какими бункерами вы хотите играть'}
-                  renderSelectedText={renderBunkerSelectedText}
-                  items={sheltersCategories}
-                  uniqueKey={'name'}
-                  subKey={'children'}
-                  displayKey={'name'}
-                  selectText={'Выбрать бункер'}
-                  selectedByDefault={['Тора Бора']}
-                  searchPlaceholderText={'Искать Бункеры'}
-                  onValueChange={shelterNames =>
-                    gameSettingsStore.setSettings(options => {
-                      options.shelters = shelterNames.map(shelterName => shelterNameToShelter(shelterName));
-                    })
-                  }
-                />
-                <GameOptionList<ApocalypseCategories>
-                  title={'Список апокалипсисов'}
-                  descriptionHeight={100}
-                  description={'Выберите из списка, с какими апокалипсисами вы хотите играть'}
-                  renderSelectedText={renderApocalypseSelectedText}
-                  items={apocalypsesCategories}
-                  uniqueKey={'name'}
-                  displayKey={'name'}
-                  selectedByDefault={['Гигантские Змеи']}
-                  selectText={'Выбрать Апокалипсис'}
-                  searchPlaceholderText={'Искать Апокалипсисы'}
-                  onValueChange={apocalypsesNames =>
-                    gameSettingsStore.setSettings(options => {
-                      options.apocalypses = apocalypsesNames.map(apocalypseName =>
-                        apocalypseNameToApocalypse(apocalypseName),
-                      );
-                    })
-                  }
-                  subKey={'children'}
-                />
-                <GameOptionList<CharacteristicCardsList>
-                  title={'Список используемых карточек'}
-                  descriptionHeight={100}
-                  description={'Выберите из списка, с какими карточками вы хотите играть'}
-                  renderSelectedText={renderCharacteristicCardSelectedText}
-                  items={characteristicCardsList}
-                  uniqueKey={'name'}
-                  displayKey={'name'}
-                  selectText={'Выбрать карточки'}
-                  subKey={'children'}
-                  selectedByDefault={cardKitToCards(characteristicCards).map(card => card.name)}
-                  searchPlaceholderText={'Искать карточки'}
-                  onValueChange={characteristicCardsNames =>
-                    gameSettingsStore.setSettings(options => {
-                      options.cardsKit = characteristicCardsNames.map(cardName =>
-                        characteristicCardNameToCard(cardName),
-                      );
-                    })
-                  }
-                />
+                {isPageFullLoadingStarted && <>
+
+
+                  <GameOptionRange
+                    title={'Уровень сложности'}
+                    defaultValue={settings.difficulty}
+                    description={'Параметр определяет насколько персонажи полезны и безопасны в среднем'}
+                    descriptionHeight={100}
+                    selectedTitle={difficultyValueToTitle(gameSettingsStore.settings.difficulty)}
+                    onValueChanged={difficulty =>
+                      gameSettingsStore.setSettings(options => (options.difficulty = difficulty))
+                    }
+                    min={gameSettingsStore.settingsLimits.difficulty.min}
+                    max={gameSettingsStore.settingsLimits.difficulty.max}
+                  />
+                  <GameOptionRange
+                    title={'Баланс Персонажей'}
+                    descriptionHeight={100}
+                    description={'Параметр определяет насколько различается полезность персонажей'}
+                    onValueChanged={characterBalance =>
+                      gameSettingsStore.setSettings(options => (options.balance = characterBalance))
+                    }
+                    selectedTitle={characterBalanceValueToTitle(settings.balance)}
+                    min={gameSettingsStore.settingsLimits.balance.min}
+                    max={gameSettingsStore.settingsLimits.balance.max}
+                    defaultValue={settings.balance}
+                  />
+                  <GameOptionRange
+                    title={'Баланс характеристик'}
+                    descriptionHeight={70}
+                    description={'Параметр определяет разброс характеристик'}
+                    onValueChanged={characteristicBalance =>
+                      gameSettingsStore.setSettings(settings => (settings.characteristicBalance = characteristicBalance))
+                    }
+                    selectedTitle={characteristicBalanceValueToTitle(settings.characteristicBalance)}
+                    min={gameSettingsStore.settingsLimits.characteristicBalance.min}
+                    max={gameSettingsStore.settingsLimits.characteristicBalance.max}
+                    defaultValue={settings.characteristicBalance}
+                  />
+
+                  <GameOptionList<ShelterCategoryList>
+                    title={'Список бункеров'}
+                    descriptionHeight={100}
+                    description={'Выберите из списка, с какими бункерами вы хотите играть'}
+                    renderSelectedText={renderBunkerSelectedText}
+                    items={sheltersCategories}
+                    uniqueKey={'name'}
+                    subKey={'children'}
+                    displayKey={'name'}
+                    selectText={'Выбрать бункер'}
+                    selectedByDefault={['Тора Бора']}
+                    searchPlaceholderText={'Искать Бункеры'}
+                    onValueChange={shelterNames =>
+                      gameSettingsStore.setSettings(options => {
+                        options.shelters = shelterNames.map(shelterName => shelterNameToShelter(shelterName));
+                      })
+                    }
+                  />
+                  <GameOptionList<ApocalypseCategories>
+                    title={'Список апокалипсисов'}
+                    descriptionHeight={100}
+                    description={'Выберите из списка, с какими апокалипсисами вы хотите играть'}
+                    renderSelectedText={renderApocalypseSelectedText}
+                    items={apocalypsesCategories}
+                    uniqueKey={'name'}
+                    displayKey={'name'}
+                    selectedByDefault={['Гигантские Змеи']}
+                    selectText={'Выбрать Апокалипсис'}
+                    searchPlaceholderText={'Искать Апокалипсисы'}
+                    onValueChange={apocalypsesNames =>
+                      gameSettingsStore.setSettings(options => {
+                        options.apocalypses = apocalypsesNames.map(apocalypseName =>
+                          apocalypseNameToApocalypse(apocalypseName),
+                        );
+                      })
+                    }
+                    subKey={'children'}
+                  />
+                  <GameOptionList<CharacteristicCardsList>
+                    title={'Список используемых карточек'}
+                    descriptionHeight={100}
+                    description={'Выберите из списка, с какими карточками вы хотите играть'}
+                    renderSelectedText={renderCharacteristicCardSelectedText}
+                    items={characteristicCardsList}
+                    uniqueKey={'name'}
+                    displayKey={'name'}
+                    selectText={'Выбрать карточки'}
+                    subKey={'children'}
+                    selectedByDefault={cardKitToCards(characteristicCards).map(card => card.name)}
+                    searchPlaceholderText={'Искать карточки'}
+                    onValueChange={characteristicCardsNames =>
+                      gameSettingsStore.setSettings(options => {
+                        options.cardsKit = characteristicCardsNames.map(cardName =>
+                          characteristicCardNameToCard(cardName),
+                        );
+                      })
+                    }
+                  />
+                </>}
+
               </ScrollView>
             </SafeAreaView>
           </View>
